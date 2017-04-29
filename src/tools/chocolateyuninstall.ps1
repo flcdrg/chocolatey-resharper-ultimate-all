e8c112ce72e9d8fce7df82a82d5e0973a380dba7
David Gardiner <david@gardiner.net.au>
2017-04-29 13:35:41 +0930

    Add logo
    - Fix uninstall

--- src/tools/chocolateyuninstall.ps1
+++ src/tools/chocolateyuninstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'; # stop on all errors
 
 $platformPackageName = 'resharper-platform'
-$packageName = 'ReSharper'
+$packageName = 'ReSharper-Ultimate-All'
 $scriptPath = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 $commonPath = $(Split-Path -parent $(Split-Path -parent $scriptPath))
 $installPath = Join-Path  (Join-Path $commonPath $platformPackageName) 'JetBrains.ReSharperUltimate.2017.1.2.exe'
-# Uninstall-ChocolateyPackage $packageName 'exe' '/Silent=True /SpecificProductNamesToRemove=ReSharper /VsVersion=* /ReSharper9PlusMsi=True' $installPath
+Uninstall-ChocolateyPackage $packageName 'exe' '/Silent=True /SpecificProductNamesToRemove=ReSharper;dotTrace;dotMemory;dotCover;dotPeek /VsVersion=* /ReSharper9PlusMsi=True' $installPath
