eab830b7b2eeb7f305face786de5cce63fe766af
David Gardiner <david@gardiner.net.au>
2017-09-22 10:54:01 +0930

    Update for 2017.2.1
    - Stop ETW process if running on upgrade/uninstall
    - Add package parameter /PerMachine

--- src/tools/chocolateyinstall.ps1
+++ src/tools/chocolateyinstall.ps1
@@ -4,13 +4,23 @@ $platformPackageName = 'resharper-platform'
 $packageName = 'ReSharper-Ultimate-All'
 $scriptPath = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 $commonPath = $(Split-Path -parent $(Split-Path -parent $scriptPath))
-$installPath = Join-Path  (Join-Path $commonPath $platformPackageName) 'JetBrains.ReSharperUltimate.2017.1.3.exe'
+$installPath = Join-Path  (Join-Path $commonPath $platformPackageName) "JetBrains.ReSharperUltimate.$($env:ChocolateyPackageVersion).exe"
+
+$silentArgs = "/Silent=True /SpecificProductNames=ReSharper;dotTrace;dotMemory;dotCover;dotPeek /VsVersion=*"
+
+$packageParameters = Get-PackageParameters
+
+if ($packageParameters["PerMachine"] -ne $null) {
+  $silentArgs += " /PerMachine=True"
+}
+
 $packageArgs = @{
   packageName   = $packageName
   fileType      = 'exe'
   file          = $installPath
-  silentArgs    = "/Silent=True /SpecificProductNames=ReSharper;dotTrace;dotMemory;dotCover;dotPeek /VsVersion=*"
+  silentArgs    = $silentArgs
   validExitCodes= @(0)
   softwareName  = 'ReSharper'
 }
+
 Install-ChocolateyInstallPackage @packageArgs
--- src/tools/chocolateyuninstall.ps1
+++ src/tools/chocolateyuninstall.ps1
@@ -4,5 +4,13 @@ $platformPackageName = 'resharper-platform'
 $packageName = 'ReSharper-Ultimate-All'
 $scriptPath = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 $commonPath = $(Split-Path -parent $(Split-Path -parent $scriptPath))
-$installPath = Join-Path  (Join-Path $commonPath $platformPackageName) 'JetBrains.ReSharperUltimate.2017.1.3.exe'
-Uninstall-ChocolateyPackage $packageName 'exe' '/Silent=True /SpecificProductNamesToRemove=ReSharper;dotTrace;dotMemory;dotCover;dotPeek /VsVersion=* /ReSharper9PlusMsi=True' $installPath
+$installPath = Join-Path  (Join-Path $commonPath $platformPackageName) "JetBrains.ReSharperUltimate.$($env:ChocolateyPackageVersion).exe"
+$silentArgs = '/Silent=True /SpecificProductNamesToRemove=ReSharper;dotTrace;dotMemory;dotCover;dotPeek /VsVersion=* /ReSharper9PlusMsi=True'
+
+$packageParameters = Get-PackageParameters
+
+if ($packageParameters["PerMachine"] -ne $null) {
+  $silentArgs += " /PerMachine=True"
+}
+
+Uninstall-ChocolateyPackage $packageName 'exe' $silentArgs $installPath
