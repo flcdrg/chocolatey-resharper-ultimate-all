42cebe5be6cc9dff5f26ad0cdbb46d2e3450893d
David Gardiner <david@gardiner.net.au>
2018-02-05 12:15:55 +1030

    2017.3.2

--- src/tools/chocolateyuninstall.ps1
+++ src/tools/chocolateyuninstall.ps1
@@ -5,6 +5,7 @@ $packageName = 'ReSharper-Ultimate-All'
 $scriptPath = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 $commonPath = $(Split-Path -parent $(Split-Path -parent $scriptPath))
 $installPath = Join-Path  (Join-Path $commonPath $platformPackageName) "JetBrains.ReSharperUltimate.$($env:ChocolateyPackageVersion).exe"
+
 $silentArgs = '/Silent=True /SpecificProductNamesToRemove=ReSharper;dotTrace;dotMemory;dotCover;dotPeek /VsVersion=* /ReSharper9PlusMsi=True'
 
 $packageParameters = Get-PackageParameters
